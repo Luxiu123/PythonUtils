b1bd683a47f548832a4c59f8cddcf4cdff3c2ce2
Luxiu123 <13467486215@163.com>
2021-08-15 07:58:38 +0800

    perf: ChineseWordTransform增加用户输入

--- main/ChineseWordTransform.py
+++ main/ChineseWordTransform.py
@@ -3,9 +3,12 @@
 中文简体繁体转换
 """
 import json
+import Utils
+import os
+
 trad_simp_map = {}  # {繁体: 简体}
 simp_trad_map = {}  # {简体: 繁体}
-with open('./WordMap.json', 'r', encoding='utf-8') as f:
+with open("./WordMap.json", "r", encoding="utf-8") as f:
     trad_simp_map = json.loads(f.read())
 for key, val in trad_simp_map.items():
     simp_trad_map[val] = key
@@ -34,9 +37,19 @@ class Transform:
         word_arr = []
         for word in text:
             word_arr.append(word_map.get(word, word))
-        return ''.join(word_arr)
+        return "".join(word_arr)
 
 
 if __name__ == "__main__":
-    text = "这是一段测试用例"
-    print(Transform.simplified_to_traditional(text))
+    path = input("输入文件路径：").strip()
+    while not Utils.is_path_valid(path):
+        print("路径无效！")
+        path = input("输入文件路径：").strip()
+    dirname, filename = os.path.split(path)
+    suffix = os.path.splitext(filename)
+    with open(path, mode="rt", encoding="utf-8") as f:
+        text = f.read()
+    dist_path = os.path.join(dirname, f"dist{suffix[1]}")
+    with open(dist_path, mode="wt", encoding="utf-8") as f:
+        f.write(Transform.traditional_to_simplified(text))
+    print(f"finished at {dist_path}")
